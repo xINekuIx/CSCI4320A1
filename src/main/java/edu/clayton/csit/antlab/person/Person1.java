bdddbb18cf8f8f20260efb3f989dbcacebe4c00b
xINekuIx <matthewtran1995@gmail.com>
2022-09-02 19:53:34 -0400

    Update Person1.java
    Implemented calc method

--- src/main/java/edu/clayton/csit/antlab/person/Person1.java
+++ src/main/java/edu/clayton/csit/antlab/person/Person1.java
@@ -31,6 +31,7 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
+		String output = input.substring(2) + input.substring(0, 2);
 	  return null;
 	}
 	
